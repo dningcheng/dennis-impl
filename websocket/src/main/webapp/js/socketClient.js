f908ced0650bfe00d6fe5c8a4d0df3b463ebf5bb
dnc <dningcheng@163.com>
2017-09-01 19:47:12 +0800

    聊天界面

--- websocket/src/main/webapp/js/socketClient.js
+++ websocket/src/main/webapp/js/socketClient.js
@@ -7,20 +7,52 @@ function User(UID,name,nickName,img){
 	this.img=img;
 	
 }
-function MyWebSocket(wsurl,UID){
+function MyWebSocket(wsurl,identity){
+	this.wsurl=wsurl;
+	this.identity=identity;
 	this.websocket=null;
 	this.connectServer=function(){
 		if(websocket==null){
-			websocket=new WebSocket(wsurl+"/{"+UID+"}");
+			websocket=new WebSocket(wsurl+"/{"+this.identity+"}");
 		}
 		if(websocket.readyState==1){
 			//正常进行连接中
 		}
+		
+		this.connected=function(event){
+			//连接成功
+			alert("连接成功！");
+		}
+		this.receiveMessage=function(message){
+			
+		}
+		//连接发生错误的回调方法
+		this.websocket.onerror=function(){
+			if(confirm("连接异常是否进行重连！")){
+				connectServer();
+			}
+		};
+		
+		//连接成功建立的回调方法
+		this.websocket.onopen = function(event){
+			this.connected(event);
+		}
+		 
+		//接收到消息的回调方法
+		this.websocket.onmessage = function(event){
+			this.receiveMessage(event.data);
+		}
+		 
+		//连接关闭的回调方法
+		this.websocket.onclose = function(){
+		    this.userLeave(this.identity);
+		}
 	};
-	this.e
-	websocket.onerror=function(){
-		connectServer();
-	};
+	
+	//监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
+	window.onbeforeunload = function(){
+		this.websocket.close();
+	}
 }
 MyWebSocket.prototype = {
 	constructor:MyWebSocket,
@@ -39,7 +71,9 @@ MyWebSocket.prototype = {
 	userJoin : function(user){
 		if(typeof(user)=="User"){
 			this.users.push(user);
+			this.connectServer();
 		}
+		
 	},
 	userLeave: function(UID){
 		for(i=0;i<users.length;i++){
@@ -48,9 +82,6 @@ MyWebSocket.prototype = {
 				users.pop();
 			}
 		}
-		this.closeWebsocket();
-	},
-	closeWebsocket:function(){
 		this.websocket.close();
 	}
 	
